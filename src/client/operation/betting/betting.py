06f420e172cac0588fb3d7b1c0fd93d3be7d9114
Jiawei Wu <Jiawei.Wu@betfair.com>
2016-05-28 17:55:31 +0100

    Restructure modules.

--- src/client/operation/betting/betting.py
+++ src/client/operation/betting/betting.py
@@ -1,4 +1,5 @@
 from client.operation.operation import Operation
+from client.utils.utils import serialise
 from utils.configs import Configs
 from utils.sessionmanager import SessionManager
 
@@ -8,7 +9,8 @@ class Betting(Operation):
         super().__init__(configs.betting_endpoint, session_manager)
 
     def place_orders(self, market_id: str, instructions: list(), customer_ref: str = None):
-        pass
+        order = Betting.Order(market_id, instructions, customer_ref)
+        self._call_api(serialise(order))
 
     def cancel_orders(self, market_id: str = None, instruction: list() = None, customer_ref: str = None):
         pass
@@ -18,3 +20,9 @@ class Betting(Operation):
 
     def update_orders(self, market_id: str, instructions: list(), customer_ref: str = None):
         pass
+
+    class Order:
+        def __init__(self, market_id: str, instructions: list(), customer_ref: str = None):
+            self.market_id = market_id
+            self.instructions = instructions
+            self.customer_ref = customer_ref
--- src/client/operation/operation.py
+++ src/client/operation/operation.py
@@ -8,5 +8,7 @@ class Operation:
         self._endpoint = endpoint
         self._session_manager = session_manager
 
-    def _call_api(self, headers, data):
+    def _call_api(self, data):
+        headers = {"Accept": "application/json", "X-Application": self._session_manager.get_session()}
         req = requests.post(self._endpoint, headers=headers, data=data)
+        print(req.json())
